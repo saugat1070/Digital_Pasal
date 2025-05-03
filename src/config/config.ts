32eb1595c2372fe887dfe74ffd1f727f64f3da97
saugat1070 <saugatgiri1070@gmail.com>
2025-05-04 02:17:27 +0545

    config varibales

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,5 +3,9 @@ config()
 
 export const envConfig = {
     port : process.env.PORT_NUMBER,
-    db_uri : process.env.DATA_BASE_URI
+    db_uri : process.env.DATA_BASE_URI,
+    jwtsecretkey : process.env.JWT_SECRET_KEY,
+    jwtExpireIn : process.env.JWT_EXPIRE_IN,
+    emailUser : process.env.EMAIL,
+    emailPassword : process.env.EMAIL_APP_PASSWORD as string,
 }
