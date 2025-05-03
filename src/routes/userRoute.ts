490fee09d5711de5fa831b078565ce8c2e92f285
saugat1070 <saugatgiri1070@gmail.com>
2025-05-03 22:59:36 +0545

    userroute for login include

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -4,6 +4,6 @@ import UserController from "../Controller/userController";
 const router : Router = express.Router()
 
 router.route('/register').post(UserController.register)
-
+router.route('/login').post(UserController.Login)
 
 export default router;
