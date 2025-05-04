acb560146a0f4f75d9163858da5218adf4c74322
saugat1070 <saugatgiri1070@gmail.com>
2025-05-05 00:14:31 +0545

    adding routes for forget password,reset password and otp verifaction

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -3,7 +3,11 @@ import UserController from "../Controller/userController";
 
 const router : Router = express.Router()
 
-router.route('/register').post(UserController.register)
-router.route('/login').post(UserController.Login)
-router.route('/forgetpassword').post(UserController.handleForgotPassword)
+router.route('/register').post(UserController.register);
+router.route('/login').post(UserController.Login);
+router.route('/forgetpassword').post(UserController.handleForgotPassword);
+router.route('/otprequest').post(UserController.verifyOtp);
+router.route('/resetpassword').post(UserController.resetPassword);
+
+
 export default router;
