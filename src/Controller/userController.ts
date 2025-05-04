8b149814cd464993c36979b80ff7ddc132f1a447
saugat1070 <saugatgiri1070@gmail.com>
2025-05-05 00:13:46 +0545

    adding verify otp and reset method in userController

--- src/Controller/userController.ts
+++ src/Controller/userController.ts
@@ -5,6 +5,9 @@ import bcrypt from 'bcrypt'
 import generateToken from "../services/generateToken";
 import generateOtp from "../services/otpGenerate";
 import sendMail from "../services/mailSender";
+import findData from "../services/findData";
+import otpExp from "../services/otpExpire";
+import sendResponse from "../services/sendResponse";
 class UserController{
     public static async register(req:Request,res:Response){
         const {username,email,password} = req.body;
@@ -34,6 +37,16 @@ class UserController{
             email : email,
             password : bcrypt.hashSync(password,10)
         })
+
+        try {
+            await sendMail({
+                to:email,
+                subject:"Register Account",
+                text : "User Account Register successfully" 
+            })
+        } catch (error) {
+            console.log(error)
+        }
 
         res.status(201).json({
             message : "User is Created Successfully",
@@ -133,16 +146,99 @@ class UserController{
                 text : `you just request to change reset password.here is you otp, ${otp}`
             })
         } catch (error) {
-            console.log(error)
-            
+            console.log(error)  
         }
 
+        user.otp = otp.toString()
+        user.otptime = Date.now().toString()
+        await user.save()
+
         res.status(200).json({
-            message : "otp is pathae deko cha"
+            message : "otp  pathae deko cha"
         })
 
 
     }
+
+    static async verifyOtp(req:Request,res:Response){
+        const {email,otp} = req.body;
+        if(!email || !otp){
+            res.status(400).json({
+                message : "Please enter email and otp"
+            })
+            return;
+        }
+
+        /* const [data] = await User.findAll({
+            where:{
+                otp : otp,
+                email : email
+            }
+        }) */
+       const [user] = await User.findAll({
+        where:{
+            email: email
+        }
+       })
+       if(!user){
+        res.status(404).json({
+            message : "User with this email is not found"
+        })
+       }
+
+       const data = await findData(User,{
+        otp : otp,
+        email : email
+       })
+
+       otpExp(data,otp,res);
+
+
+       if(!data){
+        res.status(404).json({
+            message : "Invalid Otp"
+        })
+        return;
+       }
+
+    }
+    
+    static async resetPassword(req:Request,res:Response){
+        const {newPassword,confirmPassword,email} = req.body
+        if(!newPassword || !confirmPassword || !email){
+            res.status(400).json({
+                message : "please enter all data"
+            })
+            return
+        }
+
+        if(newPassword !== confirmPassword){
+            res.status(403).json({
+                message : "newPassword and confirm password should be same"
+            })
+            return;
+        }
+
+        const user = await findData(User,{
+            email:email
+        })
+
+        if(!user){
+            sendResponse(res,404,"User with this email is not found");
+            return;
+        }
+        user.password = bcrypt.hashSync(newPassword,10);
+        await user.save();
+        sendResponse(res,200,"Password reset success")
+
+    
+
+
+
+
+    }
+
+    
 }
 
 export default UserController;
