80b00e8cafe1854b9885651f73ee5bf4cbbe863e
saugat1070 <saugatgiri1070@gmail.com>
2025-05-06 03:38:14 +0545

    add function call for adminseefer

--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
+import adminUser from "./adminSeeder";
 import app from "./src/app";
 import { envConfig } from "./src/config/config";
+import CategoryController from "./src/Controller/categoryController";
 
 function startServer(){
     const port = envConfig.port || 4000;
-
+    adminUser();
+    CategoryController.seedCategory()
     app.listen(port,()=>{
         console.log(`Server is started at ${port}`);
     })
