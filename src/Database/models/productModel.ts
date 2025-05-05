0d2a02d6d0008c6a356c25787edd3152dda512b2
saugat1070 <saugatgiri1070@gmail.com>
2025-05-06 03:36:05 +0545

    making tables for product

---
+++ src/Database/models/productModel.ts
@@ -0,0 +1,50 @@
+import { Table,Column,Model,DataType, PrimaryKey, AllowNull } from "sequelize-typescript";
+
+
+@Table({
+   tableName : "productTable",
+   modelName : "Product",
+   timestamps : true
+})
+
+class Product extends Model{
+    @Column({
+        primaryKey: true,
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4
+    })
+    declare id:string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull : false
+    })
+    declare productName:string
+
+    @Column({
+        type: DataType.TEXT,
+    })
+    declare productDescription:string
+
+    @Column({
+        type: DataType.FLOAT,
+        allowNull : false
+    })
+    declare productPrice:number
+
+    @Column({
+        type: DataType.INTEGER
+    })
+    declare productQuantity:number
+
+    @Column({
+    type : DataType.INTEGER
+    })
+    declare productDiscount : number
+
+
+
+
+}
+
+export default Product;
