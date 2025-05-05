c7b572af2028b5af1aa3bb6a4c1a5a49e3c52fdd
saugat1070 <saugatgiri1070@gmail.com>
2025-05-06 03:38:56 +0545

    adding middleware for router

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import "./Database/db_connect";
 const app: Application = express()
 app.use(express.json());
 import userRoute from './routes/userRoute'
+import categoryRoute from './routes/categoryRoute'
 
 
 //for route
 app.use('/api/auth',userRoute); //localhost:3000/api/auth/register
+app.use('/api',categoryRoute); //localhost:3000/api/
 
 
 
