a4f89ed89eda2b0866425a5b67d758c3c6730b04
saugat1070 <saugatgiri1070@gmail.com>
2025-05-06 15:03:58 +0545

    when we verify jwt token, jwt.verify reads only reads token, so we have to remove Bearer from sentence

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -4,7 +4,10 @@ import { envConfig } from '../config/config';
 class UserMiddleware{
     async IsUserLoggeIn(req:Request,res:Response,next:NextFunction):Promise<void>{
         // receive
-        const token = req.headers.authorization
+        const authHeader = req.headers.authorization; //it reads in {Bearer token} and we have to remove Bearer
+        // const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
+        const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
+        console.log(token);
         if(!token){
             res.status(403).json({
                 message : "Token must be provided"
@@ -22,7 +25,7 @@ class UserMiddleware{
             else{
                 console.log("valid token")
                 console.log(result); //{userid}
-                next()
+                next();
             }
         })
 
