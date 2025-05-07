728abd4ea29337411cd108b9eefb57faed4ce8fe
saugat1070 <saugatgiri1070@gmail.com>
2025-05-07 21:18:10 +0545

    adding role middleware function

--- src/middleware/userMiddleware.ts
+++ src/middleware/userMiddleware.ts
@@ -1,8 +1,26 @@
 import {Request,Response,NextFunction} from 'express'
 import jwt from "jsonwebtoken"
 import { envConfig } from '../config/config';
+import User from '../Database/models/userModel';
+
+enum Role{
+    Admin = "admin",
+    Customer = "customer"
+}
+
+interface IExtendedRequest extends Request{
+    user? : {
+        username : string,
+        email : string,
+        role : string,
+        password : string,
+        id : string
+
+    }
+
+}
 class UserMiddleware{
-    async IsUserLoggeIn(req:Request,res:Response,next:NextFunction):Promise<void>{
+    async IsUserLoggeIn(req:IExtendedRequest,res:Response,next:NextFunction):Promise<void>{
         // receive
         const authHeader = req.headers.authorization; //it reads in {Bearer token} and we have to remove Bearer
         // const token = authHeader && authHeader.startsWith("Bearer ") ? authHeader.split(" ")[1] : null;
@@ -15,7 +33,7 @@ class UserMiddleware{
             return;
         }
 
-        jwt.verify(token,envConfig.jwtsecretkey as string, async (err,result)=>{
+        jwt.verify(token,envConfig.jwtsecretkey as string, async (err,result:any)=>{
             if(err){
                 console.log(err);
                 res.status(403).json({
@@ -25,12 +43,41 @@ class UserMiddleware{
             else{
                 console.log("valid token")
                 console.log(result); //{userid}
+
+                const user_data = await User.findByPk(result?.userId);
+                // //@ts-ignore
+                // req.id = result?.userid
+                if(!user_data){
+                    res.status(404).json({
+                        message : "user is not found"
+                    })
+                    return
+                }
+                req.user = user_data;
                 next();
             }
         })
 
         //validate
     }
+
+    accessTo(...roles: Role[]){ //['admin','customer']
+        // let userRole = req.user.role as Role
+        return (req:IExtendedRequest,res:Response,next:NextFunction)=>{
+            let userRole = req.user?.role as Role
+            console.log(userRole)
+            console.log(userRole)
+            if(!roles.includes(userRole)){
+                res.status(403).json({
+                    message : "user must be admin"
+                })
+            }
+            else{
+                next();
+            }
+        }
+    }
+
 
 
 }
