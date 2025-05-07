3930efeae332f96c50fdb34866d157a000d07043
saugat1070 <saugatgiri1070@gmail.com>
2025-05-07 21:17:08 +0545

    adding accessTo role middleware

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -4,7 +4,11 @@ import userMiddleware from "../middleware/userMiddleware";
 // const router = new Router()
 const router : Router = express.Router()
 
+enum Role{
+    Admin = "admin",
+    Customer = "customer"
+}
 // router.route('/seedCategory').post(categoryController.seedCategory)
-router.route('/category').get(userMiddleware.IsUserLoggeIn,CategoryController.getCategory).post(CategoryController.addCategory)
+router.route('/category').get(CategoryController.getCategory).post(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin),CategoryController.addCategory)
 router.route('/category/:id').patch(CategoryController.updateCategory).delete(CategoryController.deleteCategory)
 export default router
