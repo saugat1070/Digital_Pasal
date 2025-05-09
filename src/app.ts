32e10d949e0c3b253e8172218ecb2e3f1ed60f58
saugat1070 <saugatgiri1070@gmail.com>
2025-05-09 17:14:52 +0545

    update on app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ const app: Application = express()
 app.use(express.json());
 import userRoute from './routes/userRoute'
 import categoryRoute from './routes/categoryRoute'
-
+import productRoute from './routes/productRoute'
 
 //for route
 app.use('/api/auth',userRoute); //localhost:3000/api/auth/register
 app.use('/api',categoryRoute); //localhost:3000/api/
+app.use('/api/product',productRoute); //localhost:3000/api/product
 
 
 
