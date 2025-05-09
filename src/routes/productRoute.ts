c83559ab9564171eff7e4c2ed8f9b1ae5af13807
saugat1070 <saugatgiri1070@gmail.com>
2025-05-09 23:36:47 +0545

    adding error handling for each route

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -3,18 +3,20 @@ import { ProductController } from "../Controller/productController";
 import userMiddleware, {Role} from "../middleware/userMiddleware";
 import { multer,storage } from "../middleware/multer";
 import { UpdatedAt } from "sequelize-typescript";
+import { errorHandler } from "../services/errorHandler";
 
 const upload = multer({storage:storage})
 
 const router : Router = express.Router()
 
 router.route("/")
-.post(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin),ProductController.createProduct)
-.get(ProductController.getAllProduct)
+.post(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin),upload.single("productImage"),errorHandler(ProductController.createProduct))
+.get(errorHandler(ProductController.getAllProduct));
 
 
 router.route("/:id")
-.patch(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin) ,ProductController.UpdateProduct)
-.delete(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin) ,ProductController.deleteProduct)
+.get(ProductController.getSingleData)
+.patch(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin) ,errorHandler(ProductController.UpdateProduct))
+.delete(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin) ,errorHandler(ProductController.deleteProduct))
 
 export default router;
