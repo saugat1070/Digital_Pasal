e06a34327c0f6b1afa03b31823b01a49aa841fd8
saugat1070 <saugatgiri1070@gmail.com>
2025-05-09 23:35:09 +0545

    making file for global uses

---
+++ src/globals/types.ts
@@ -0,0 +1,18 @@
+export enum orderStatus{
+    pending = "pending",
+    cancelled = "cencelled",
+    ontheway = "ontheway",
+    delivered = "delivered",
+    preparation = "preparation"
+}
+
+export enum paymentMethod{
+    khalti = "khalti",
+    esewa = "esewa",
+    COD = "COD"
+}
+
+export enum paymentStatus{
+    paid = "paid",
+    unpaid = "unpaid"
+}
