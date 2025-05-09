7a9f2c6f961d8a0fa82ac6676e516397388b66c0
saugat1070 <saugatgiri1070@gmail.com>
2025-05-09 17:13:33 +0545

    update on multer middleware

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 import { Request } from "express";
 const storage = multer.diskStorage({
     destination:(req:Request,file:Express.Multer.File,cb:any)=>{
-        cb(null,'./storage')
+        cb(null,'./src/storage')
     },
     filename : (req:Request,file:Express.Multer.File,cb:any)=>{
         cb(null,Date.now+"-"+file.originalname)
