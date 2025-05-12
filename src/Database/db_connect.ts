df29c9b68a4b9074c0e5cca74cb58a3ec6bfe082
saugat1070 <saugatgiri1070@gmail.com>
2025-05-12 18:59:29 +0545

    update on relationship between model

--- src/Database/db_connect.ts
+++ src/Database/db_connect.ts
@@ -5,10 +5,12 @@ import Product from "./models/productModel";
 import Category from "./models/categoryModel";
 import Order from "./models/orderModel";
 import Payment from "./models/paymentModel";
+import OrderDetails from "./models/orderDetailModel";
+import Cart from "./models/cartModel";
 const sequelize = new Sequelize(envConfig.db_uri as string,
     {
         dialect : 'postgres',
-        models : [User,Product,Category,Order,Payment]
+        models : [User,Product,Category,Order,Payment,OrderDetails,Cart]
     }
 );
 
@@ -24,14 +26,39 @@ try {
     console.log(error);    
 }
 
- sequelize.sync({force: false,alter:true}).then(()=>{
+ sequelize.sync({force: false,alter:false}).then(()=>{
     console.log("synced!!");
  })
 
  //relationships //
- Product.belongsTo(Category,{foreignKey:'categoryId'})
  Category.hasOne(Product,{foreignKey:'categoryId'})
+ Product.belongsTo(Category,{foreignKey:'categoryId'})
 // Category.belongsTo(Product) -> yesma Catgory table maa product id link huncha
 // Product.hasOne(Category)
+
+//user X order
+User.hasMany(Order,{foreignKey:'userId'})
+Order.belongsTo(User,{foreignKey:'userId'})
+
+//Order X payment
+Order.hasOne(Payment,{foreignKey:"orderId"})
+Payment.belongsTo(Order,{foreignKey:"orderId"})
+
+// OrderDetails and ProductTable
+OrderDetails.belongsTo(Product,{foreignKey:"productId"})
+Product.hasMany(OrderDetails,{foreignKey:"productId"})
+
+//Order and OrderDetails
+OrderDetails.belongsTo(Order,{foreignKey:"orderId"})
+Order.hasOne(OrderDetails,{foreignKey:"orderId"})
+
+//Cart and User
+User.hasOne(Cart,{foreignKey:'userId'})
+Cart.belongsTo(User,{foreignKey:'userId'})
+
+//Cart and Product
+Product.hasMany(Cart,{foreignKey:"productId"})
+Cart.belongsTo(Product,{foreignKey:"productId"})
+
 
 export default sequelize;
