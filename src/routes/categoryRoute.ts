5fdd7eb643221065f2cea950b7e18e8f71f01778
saugat1070 <saugatgiri1070@gmail.com>
2025-05-12 18:58:02 +0545

    formating using prettier

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -1,14 +1,24 @@
-import express,{ Router } from "express";
+import express, { Router } from "express";
 import CategoryController from "../Controller/categoryController";
 import userMiddleware from "../middleware/userMiddleware";
 // const router = new Router()
-const router : Router = express.Router()
+const router: Router = express.Router();
 
-enum Role{
-    Admin = "admin",
-    Customer = "customer"
+enum Role {
+  Admin = "admin",
+  Customer = "customer",
 }
 // router.route('/seedCategory').post(categoryController.seedCategory)
-router.route('/category').get(CategoryController.getCategory).post(userMiddleware.IsUserLoggeIn,userMiddleware.accessTo(Role.Admin),CategoryController.addCategory)
-router.route('/category/:id').patch(CategoryController.updateCategory).delete(CategoryController.deleteCategory)
-export default router
+router
+  .route("/category")
+  .get(CategoryController.getCategory)
+  .post(
+    userMiddleware.IsUserLoggeIn,
+    userMiddleware.accessTo(Role.Admin),
+    CategoryController.addCategory
+  );
+router
+  .route("/category/:id")
+  .patch(CategoryController.updateCategory)
+  .delete(CategoryController.deleteCategory);
+export default router;
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,12 @@
-import express,{ Router, } from "express";
+import express, { Router } from "express";
 import UserController from "../Controller/userController";
 
-const router : Router = express.Router()
+const router: Router = express.Router();
 
-router.route('/register').post(UserController.register);
-router.route('/login').post(UserController.Login);
-router.route('/forgetpassword').post(UserController.handleForgotPassword);
-router.route('/otprequest').post(UserController.verifyOtp);
-router.route('/resetpassword').post(UserController.resetPassword);
-
+router.route("/register").post(UserController.register);
+router.route("/login").post(UserController.Login);
+router.route("/forgetpassword").post(UserController.handleForgotPassword);
+router.route("/otprequest").post(UserController.verifyOtp);
+router.route("/resetpassword").post(UserController.resetPassword);
 
 export default router;
